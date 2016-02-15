613577d4ed6bddbb0e85861f76db39f866d725e6
tyru <tyru.exe@gmail.com>
2016-02-16 01:57:33 +0900

    Transition descriptions and background images
    What I've learnt:
    
    * Do not toggle 'display' property for a transitioning element.
    * Change 'opacity' and 'position' properties instead.

--- js/index.js
+++ js/index.js
@@ -1,25 +1,38 @@
-window.onload = (function () {
+window.onload = (function() {
     'use strict';
 
-    function showIconOnHover(watchTargetClass, addClass) {
-        var ADD_TARGET_ID = 'links';
-        var target = document.getElementById(ADD_TARGET_ID);
-        var watchDOMList = document.getElementsByClassName(watchTargetClass);
-        var i;
-        for (i = 0; i < watchDOMList.length; i++) {
-            watchDOMList[i].onmouseover = function () {
-                target.classList.add(addClass);
-            };
-            watchDOMList[i].onmouseout = function () {
-                target.classList.remove(addClass);
-            };
-        }
+    var BGIMG_TARGET_ID = 'bg-icon';
+    function showIconOnHover(linkId, bgImgAddClass) {
+        var $bgImg = document.getElementById(BGIMG_TARGET_ID);
+        var $link = document.getElementById(linkId);
+        var $linkDesc = document.getElementById(linkId + "-desc");
+
+        var onMouseOver = (function onMouseOver() {
+            $bgImg.classList.add(bgImgAddClass);
+            $bgImg.classList.remove('invisible');
+            $linkDesc.classList.remove('invisible');
+            return false;
+        });
+        var onMouseOut = (function onMouseOut() {
+            $linkDesc.classList.add('invisible');
+            $bgImg.classList.add('invisible');
+            $bgImg.classList.remove(bgImgAddClass);
+            return false;
+        });
+
+        $link.addEventListener('mouseover', onMouseOver);
+        $link.addEventListener('mouseout', onMouseOut);
     }
 
+    // On mouseover/mouseout.
+    // * Load & Show background images on mouseover.
+    // * Show descriptions on mouseover.
+    // * Toggle 'invisible' class.
     showIconOnHover('github', 'github-bg-icon');
     showIconOnHover('hatenablog', 'hatenablog-bg-icon');
     showIconOnHover('hatebu', 'hatebu-bg-icon');
     showIconOnHover('twitter', 'twitter-bg-icon');
-    showIconOnHover('tumblr', 'tumblr-bg-icon');
+    showIconOnHover('tumblr-tyru', 'tumblr-bg-icon');
+    showIconOnHover('tumblr-inthesea', 'tumblr-bg-icon');
     showIconOnHover('vimorg', 'vimorg-bg-icon');
 });
