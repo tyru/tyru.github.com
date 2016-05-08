de1185e7dec4aace34246d98cfd3545d7f34a20f
tyru <tyru.exe@gmail.com>
2016-05-08 21:07:49 +0900

    Add <tyru-loading-bounce-icon>

---
+++ component/tyru-loading-bounce-icon/tyru-loading-bounce-icon.js
@@ -0,0 +1,8 @@
+(function() {
+  'use strict';
+
+  angular.module('tyru')
+    .component('tyruLoadingBounceIcon', {
+      templateUrl: '/component/tyru-loading-bounce-icon/tyru-loading-bounce-icon.html'
+    });
+})();
