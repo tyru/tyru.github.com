e5cd766bcd5b3e92b0802d47e880673ee904fdcd
tyru <tyru.exe@gmail.com>
2016-06-06 15:42:06 +0900

    Split css3test/ to indivisual repo

--- component/tyru-loading-bounce-icon/tyru-loading-bounce-icon.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-
-  angular.module('tyru')
-    .component('tyruLoadingBounceIcon', {
-      templateUrl: '/component/tyru-loading-bounce-icon/tyru-loading-bounce-icon.html'
-    });
-})();
